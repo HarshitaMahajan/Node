f52a44385930ff18dd434e0795306dc30cd20ed3
HarshitaMahajan <51989422+HarshitaMahajan@users.noreply.github.com>
2019-08-20 07:53:42 -0700

    Add files via upload

---
+++ assignment 1/Members.js
@@ -0,0 +1,34 @@
+const member = [
+    {
+        id: 1,
+        name: 'grishma',
+        email: 'grishma@gmail.com',
+        status: 'active'
+      },
+
+      {
+        id: 1,
+        name: 'harsh',
+        email: 'harsh22@gmail.com',
+        status: 'active'
+      },
+
+      {
+        id: 1,
+        name: 'karn',
+        email: 'karn@gmail.com',
+        status: 'active'
+      },
+
+      {
+        id: 1,
+        name: 'deepak',
+        email: 'deepak02@gmail.com',
+        status: 'active'
+      },
+     
+      
+]
+
+module.exports=member;
+
